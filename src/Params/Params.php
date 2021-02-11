54611dbe1f867f1c8a2040bf678ca3a483232b16
Kanstantsin K <kk@tolkam.by>
2021-02-11 12:39:02 +0300

    refactored Params

--- src/Params/Params.php
+++ src/Params/Params.php
@@ -2,8 +2,11 @@
 
 namespace Tolkam\Template\Params;
 
-use ArrayObject;
+use ArrayAccess;
+use ArrayIterator;
 use Closure;
+use Countable;
+use IteratorAggregate;
 use Tolkam\Utils\Arr;
 
 /**
@@ -11,7 +14,7 @@ use Tolkam\Utils\Arr;
  *
  * @package Tolkam\Template\Params
  */
-class Params extends ArrayObject implements ParamsInterface
+class Params implements IteratorAggregate, ArrayAccess, Countable
 {
     /**
      * Array path separator
@@ -21,19 +24,20 @@ class Params extends ArrayObject implements ParamsInterface
     /**
      * @var array
      */
-    private $options = [
+    private array $items = [];
     
+    /**
+     * @var array
+     */
+    private array $options = [
         // whether to use closure return value as offset value
         'resolveClosures' => true,
-    
-        // Iterator implementation class
-        'iteratorClass' => 'ArrayIterator',
     ];
     
     /**
      * @var ParamsExtensionInterface[]
      */
-    private $extensions = [];
+    private array $extensions = [];
     
     /**
      * @param array $input
@@ -41,98 +45,175 @@ class Params extends ArrayObject implements ParamsInterface
      */
     public function __construct(array $input = [], array $options = [])
     {
-        $this->options = array_merge($this->options, $options);
-        
-        // convert arrays to Params
+        $this->options = array_replace($this->options, $options);
         foreach ($input as $k => $v) {
-            if (is_array($v)) {
-                $input[$k] = new static($v, $this->options);
-            }
+            $this->items[$k] = $this->convertValue($v);
         }
+    }
+    
+    /**
+     * @param string $key
+     * @param        $value
+     *
+     * @return $this
+     */
+    public function set(string $key, $value): self
+    {
+        Arr::set($this->items, $key, $this->convertValue($value), self::PATH_SEPARATOR);
         
-        parent::__construct($input, self::ARRAY_AS_PROPS, $this->options['iteratorClass']);
-    }
-    
-    /**
-     * @inheritDoc
-     */
-    public function set(string $key, $value): void
-    {
-        Arr::set($this, $key, $value, self::PATH_SEPARATOR);
-    }
-    
-    /**
-     * @inheritDoc
-     */
-    public function add(string $key, $value): void
-    {
-        Arr::add($this, $key, $value, self::PATH_SEPARATOR);
-    }
-    
-    /**
-     * @inheritDoc
-     */
-    public function get(string $key, $default = null)
-    {
-        return Arr::get($this, $key, $default, self::PATH_SEPARATOR);
-    }
-    
-    /**
-     * @inheritDoc
-     */
-    public function toArray(): array
-    {
-        return $this->getArrayCopy();
-    }
-    
-    /**
-     * @inheritDoc
-     */
-    public function offsetSet($index, $value)
-    {
-        if (is_array($value)) {
-            $value = new static($value, $this->options);
-        }
-        
-        parent::offsetSet($index, $value);
-    }
-    
-    /**
-     * @inheritDoc
-     */
-    public function offsetGet($index)
-    {
-        return $this->resolveValue(parent::offsetGet($index));
-    }
-    
-    /**
-     * @inheritDoc
-     */
-    public function addExtension(ParamsExtensionInterface $extension): ParamsInterface
-    {
-        $this->extensions[] = $extension;
         return $this;
     }
     
     /**
-     * @inheritDoc
+     * @param string $key
+     * @param null   $default
+     *
+     * @return array|ArrayAccess|mixed
      */
-    public function withExtensions(): ParamsInterface
+    public function get(string $key, $default = null)
+    {
+        return Arr::get($this->items, $key, $default, self::PATH_SEPARATOR);
+    }
+    
+    /**
+     * @param mixed ...$values
+     *
+     * @return $this
+     */
+    public function push(...$values): self
+    {
+        array_push($this->items, ...$values);
+        
+        return $this;
+    }
+    
+    /**
+     * @param mixed ...$values
+     *
+     * @return $this
+     */
+    public function unshift(...$values): self
+    {
+        array_unshift($this->items, ...$values);
+        
+        return $this;
+    }
+    
+    /**
+     * Registers extension
+     *
+     * @param ParamsExtensionInterface $extension
+     *
+     * @return self
+     */
+    public function addExtension(ParamsExtensionInterface $extension): self
+    {
+        $this->extensions[] = $extension;
+        
+        return $this;
+    }
+    
+    /**
+     * Returns a copy of self with extensions applied
+     *
+     * @return self
+     */
+    public function withExtensions(): self
     {
         $extended = clone $this;
-    
+        
         foreach ($this->extensions as $extension) {
             $path = $extension->getPath();
-        
+            
             $currentValue = $extended->get($path);
             $newValue = $extension->extend($currentValue);
-        
+            
             if ($newValue !== $currentValue) {
                 $extended->set($path, $newValue);
             }
         }
         
         return $extended;
+    }
+    
+    /**
+     * Gets params as array
+     *
+     * @return array
+     */
+    public function toArray(): array
+    {
+        $arr = [];
+        
+        foreach ($this->items as $k => $v) {
+            $arr[$k] = $v instanceof static ? $v->toArray() : $v;
+        }
+        
+        return $arr;
+    }
+    
+    /**
+     * @inheritDoc
+     */
+    public function getIterator()
+    {
+        return new ArrayIterator($this->items);
+    }
+    
+    /**
+     * @inheritDoc
+     */
+    public function offsetSet($offset, $value)
+    {
+        $this->items[$offset] = $this->convertValue($value);
+    }
+    
+    /**
+     * @inheritDoc
+     */
+    public function offsetGet($offset)
+    {
+        return $this->resolveValue($this->items[$offset] ?? null);
+    }
+    
+    /**
+     * @inheritDoc
+     */
+    public function offsetExists($offset): bool
+    {
+        return isset($this->items[$offset]);
+    }
+    
+    /**
+     * @inheritDoc
+     */
+    public function offsetUnset($offset)
+    {
+        unset($this->items[$offset]);
+    }
+    
+    /**
+     * @inheritDoc
+     */
+    public function count(): int
+    {
+        return count($this->items);
+    }
+    
+    /**
+     * Converts value to self instance
+     *
+     * @param $value
+     *
+     * @return mixed
+     */
+    private function convertValue($value)
+    {
+        if (is_array($value)) {
+            $value = new static($value, $this->options);
+        }
+        
+        return $value;
     }
     
     /**
